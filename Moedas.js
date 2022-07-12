8949e7a4f6d102ec2a933f16c11ad273ea8a266a
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2022-07-12 16:37:38 -0300

    :recycle: refatorando

--- Moedas.js
+++ Moedas.js
@@ -1,23 +1,17 @@
     // -------------> Selecionando Elementos <---------------------
      // -------------> Selecionando input com valor digitado <---------------------
-        let Real = window.document.querySelector('#ValorDigitado').value;
-    
-    // -------------> Declaração de variáveis (Cotação do dia 12/07/2022) <---------------------
-        let Dolar = Real * 5.38;
-        let Euro = Real * 5.38;
-        let Libra = Real * 6.37;
-        let MoedaConvertida = window.document.getElementById('#Resultado') ;
-    
-        // -------------> Selecionar os elementos options dentro de select (Criar um array deles) <---------------------
-        
+       function Converter(){
+        let Real = window.document.getElementById('ValorDigitado').value 
+        let MoedaConvertida = window.document.getElementById('Resultado')
+        let Dolar = Real * 5.38
+        let DolarFormatado = Dolar.toLocaleString("en-US", {style: "currency", currency: "USD"})
 
-        // -------------> Selecionando imgs <---------------------
-        
+        MoedaConvertida.innerHTML = `Moeda Convertida USD ${DolarFormatado}`
 
-        // -------------> Selecionando as moedas <---------------------
-       
-        // -------------> Iniciando a conversão através da function <---------------------
-        function Converter(){
-           
+        if(Real < 0){
+            MoedaConvertida.innerHTML = `Por favor Digite um valor válido`
         }
+       }
+
+
 
